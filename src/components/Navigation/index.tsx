1f1944a475f00b373da701dffdec2bb95f0de688
Patrick Hettich <patrick_hettich@hotmail.com>
2022-06-12 19:54:00 +0200

    added icon

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,10 +1,11 @@
+import Icon from './Icon';
 import './index.scss';
 
 interface NavProps { };
 const Nav: React.FC<NavProps> = () => {
     return (
         <nav>
-            <div>Logo</div>
+            <div><Icon /></div>
             <ul>
                 <li><a href="#">HOME</a></li>
                 <li><a href="#">PROJECTS</a></li>
