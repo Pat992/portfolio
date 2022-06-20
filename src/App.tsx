f9093ff0d4af4479fd2e6afa0e996f950afa71f6
Patrick Hettich <patrick_hettich@hotmail.com>
2022-06-20 21:33:37 +0200

    added document loading to the project

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,30 @@ import ConnectionSidebar from './components/Sidebar/ConnectionSidebar';
 import { useAppDispatch, useAppSelector } from './store/hooks';
 import { setScrollValue } from './store/scroll-slice';
 import Footer from './components/Footer/Footer';
+import { Fragment, useEffect, useState } from 'react';
 
 const App = () => {
+  const [loaded, setLoaded] = useState(false);
   const { scrollYProgress } = useViewportScroll();
   const dispatch = useAppDispatch();
   const isDarkTheme = useAppSelector((state) => state.theme.isDarkTheme);
+
+  useEffect(() => {
+    const onPageLoad = () => {
+      setTimeout(() => {
+        setLoaded(true);
+      }, 500);
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
 
   scrollYProgress.onChange(() => {
     dispatch(setScrollValue(scrollYProgress.get()));
@@ -20,12 +39,17 @@ const App = () => {
 
   return (
     <div className={isDarkTheme ? 'dark' : 'light'}>
-      <ConnectionSidebar />
-      <Nav />
-      <Main />
-      <Projects />
-      <Skills />
-      <Footer />
+      {!loaded ?
+        <h1>loading now</h1>
+        : <Fragment>
+          <ConnectionSidebar />
+          <Nav />
+          <Main />
+          <Projects />
+          <Skills />
+          <Footer />
+        </Fragment>
+      }
     </div>
   );
 }
