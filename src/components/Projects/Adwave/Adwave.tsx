44c5b89d3b9abe93e24a148f06ea5741ba34aca2
Patrick Hettich <patrick_hettich@hotmail.com>
2022-06-20 19:11:27 +0200

    redesigned project cards

--- src/components/Projects/Adwave/Adwave.tsx
+++ src/components/Projects/Adwave/Adwave.tsx
@@ -6,12 +6,15 @@ import PublicSvg from './Public';
 interface AdWaveProps { };
 const AdWave: React.FC<AdWaveProps> = () => {
     return (
-        <div className='adwave-project'>
-            <div className='project-card adwave-card'>
-                <h2>AdWave</h2>
-                <PublicSvg />
+        <div className='adwave-project project'>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia iste quos, rem nihil obcaecati quisquam magnam neque? Aliquam, perferendis atque?</p>
+            <div className='project-card'>
+                <div className='adwave-card o-hidden'>
+                    <h2>AdWave</h2>
+                    <PublicSvg />
+                </div>
+                <InsightsSvg />
             </div>
-            <InsightsSvg />
         </div>
     );
 };
--- src/components/Projects/AndFm/And-fm.tsx
+++ src/components/Projects/AndFm/And-fm.tsx
@@ -4,11 +4,12 @@ import Background from './Bg';
 interface AndFmProps { };
 const AndFm: React.FC<AndFmProps> = () => {
     return (
-        <div className='andfm-project'>
-            <div className='project-card'>
+        <div className='andfm-project project-reverse'>
+            <div className='project-card o-hidden'>
                 <h2>AndFM</h2>
                 <Background />
             </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe pariatur dolor sequi provident quis illum nulla recusandae similique labore fugit?</p>
         </div>
     );
 };
