730cfdb2a28c23fba665e4b9240c7038e1264b5d
Patrick Hettich <patrick_hettich@hotmail.com>
2022-06-23 19:31:59 +0200

    added adwave card

--- src/components/Projects/Adwave/Adwave.tsx
+++ src/components/Projects/Adwave/Adwave.tsx
@@ -5,6 +5,7 @@ import PublicSvg from './Public';
 import phone from '../../../assets/adwave/adwave-phone.png';
 import est from '../../../assets/adwave/adwave-est.png';
 import app from '../../../assets/adwave/adwave-app.png';
+import AdwaveSvg from './AdwaveSvg';
 
 interface AdWaveProps { };
 const AdWave: React.FC<AdWaveProps> = () => {
@@ -13,11 +14,17 @@ const AdWave: React.FC<AdWaveProps> = () => {
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia iste quos, rem nihil obcaecati quisquam magnam neque? Aliquam, perferendis atque?</p>
             <div className='project-card'>
                 <div className='adwave-card o-hidden'>
-                    <h2>AdWave</h2>
+                    <div className='title-row'>
+                        <AdwaveSvg />
+                        <h2>AdWave</h2>
+                    </div>
                     <PublicSvg />
-                    <img className='project-image phone' src={phone} alt="phone" />
-                    <img className='project-image est' src={est} alt="estimate" />
-                    <img className='project-image app' src={app} alt="app" />
+                    <div className='project-image'>
+                        <img className='phone' src={phone} alt="phone" />
+                        <img className='est' src={est} alt="estimate" />
+                        <img className='app' src={app} alt="app" />
+                    </div>
+
                 </div>
                 <InsightsSvg />
             </div>
