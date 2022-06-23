8862bcd66fb84758322a10e7487d63a5842b4d3d
Patrick Hettich <patrick_hettich@hotmail.com>
2022-06-23 20:05:11 +0200

    finished andFM card

--- src/components/Projects/AndFm/AndFm.tsx
+++ src/components/Projects/AndFm/AndFm.tsx
@@ -1,12 +1,22 @@
 import '../styles.scss';
 import Background from './Bg';
+import light from '../../../assets/andfm/andfm-light.png';
+import dark from '../../../assets/andfm/andfm-dark.png';
+import AndFmSvg from './AndFmSvg';
 
 interface AndFmProps { };
 const AndFm: React.FC<AndFmProps> = () => {
     return (
         <div className='andfm-project project-reverse'>
             <div className='project-card o-hidden'>
-                <h2>AndFM</h2>
+                <div className='title-row'>
+                    <AndFmSvg />
+                    <h2>andFM</h2>
+                </div>
+                <div className='project-img'>
+                    <img className='light' src={light} alt="phone-light" />
+                    <img className='dark' src={dark} alt="phone-dark" />
+                </div>
                 <Background />
             </div>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe pariatur dolor sequi provident quis illum nulla recusandae similique labore fugit?</p>
