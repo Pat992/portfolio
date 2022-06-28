e9615910f0516f2ea19a77dffc045a8ef3e9fe1b
Patrick Hettich <patrick_hettich@hotmail.com>
2022-06-28 19:10:21 +0200

    updated project components

--- src/components/UI/IconSvg.tsx
+++ src/components/UI/IconSvg.tsx
@@ -1,16 +1,16 @@
 import { motion, Variants } from 'framer-motion';
 import './styles.scss';
 
-interface IconProps {
+interface IconSvgProps {
     animationVariants: Variants,
     onAnimationComepleteHandler: Function
 };
-const Icon: React.FC<IconProps> = ({ animationVariants, onAnimationComepleteHandler }) => {
+const IconSvg: React.FC<IconSvgProps> = ({ animationVariants, onAnimationComepleteHandler }) => {
 
     return (
-        <motion.svg variants={animationVariants} initial='from' animate='to' onAnimationComplete={() => onAnimationComepleteHandler()} className="icon" viewBox="0 0 200 200" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve">
+        <motion.svg variants={animationVariants} initial='from' animate='to' onAnimationComplete={() => onAnimationComepleteHandler()} className="IconSvg" viewBox="0 0 200 200" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve">
             <g transform="matrix(1,0,0,1,-5087,-549)">
-                <g id="icon" transform="matrix(0.104167,0,0,0.185185,5087,549)">
+                <g id="IconSvg" transform="matrix(0.104167,0,0,0.185185,5087,549)">
                     <rect x="0" y="0" width="1920" height="1080" style={{ fill: 'none' }} />
                     <g transform="matrix(9.6,0,0,5.4,-48873.6,-2951.1)">
                         <path d="M5187,570.95C5187,569.322 5185.68,568 5184.05,568C5153.11,568 5128,593.115 5128,624.05L5128,722.05C5128,723.678 5129.32,725 5130.95,725C5161.89,725 5187,699.885 5187,668.95L5187,570.95Z" className='logo' />
@@ -24,4 +24,4 @@ const Icon: React.FC<IconProps> = ({ animationVariants, onAnimationComepleteHand
     );
 };
 
-export default Icon;
+export default IconSvg;
