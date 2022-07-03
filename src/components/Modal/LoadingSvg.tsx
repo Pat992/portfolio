14290a8803af2e05b87d1445942a37ede74ae310
Patrick Hettich <patrick_hettich@hotmail.com>
2022-07-03 21:57:56 +0200

    loading, error and success animation done

--- src/components/Modal/LoadingSvg.tsx
+++ src/components/Modal/LoadingSvg.tsx
@@ -1,5 +1,6 @@
 import { useAnimation, Variants, motion } from "framer-motion";
 import { useEffect } from "react";
+import './styles.scss';
 
 const svgVariants: Variants = {
     loading: {
@@ -37,8 +38,20 @@ const errorVariants: Variants = {
         opacity: 0,
         pathLength: 0,
     },
-    success: {},
-    error: {},
+    success: {
+        opacity: 0,
+        pathLength: 0,
+        transition: {
+            duration: 0.5
+        }
+    },
+    error: {
+        opacity: 1,
+        pathLength: 1,
+        transition: {
+            duration: 0.5
+        }
+    },
 }
 
 const successVariants: Variants = {
@@ -46,17 +59,41 @@ const successVariants: Variants = {
         opacity: 0,
         pathLength: 0,
     },
-    success: {},
-    error: {},
+    success: {
+        opacity: 1,
+        pathLength: 1,
+        transition: {
+            duration: 0.5
+        }
+    },
+    error: {
+        opacity: 0,
+        pathLength: 0,
+        transition: {
+            duration: 0.5
+        }
+    },
 }
 
-interface LoadingSvgProps { };
-const LoadingSvg: React.FC<LoadingSvgProps> = () => {
+interface LoadingSvgProps {
+    isError: boolean,
+    isSuccess: boolean,
+};
+const LoadingSvg: React.FC<LoadingSvgProps> = ({ isError, isSuccess }) => {
     const controls = useAnimation();
 
     useEffect(() => {
         controls.start('loading');
     }, []);
+
+    useEffect(() => {
+        if (isSuccess) {
+            controls.start('success');
+        }
+        else if (isError) {
+            controls.start('error');
+        }
+    }, [isError, isSuccess]);
 
     return (
         <motion.svg variants={svgVariants} animate={controls} viewBox="0 0 200 201" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" xmlSpace="preserve" fillRule="evenodd" clipRule='evenodd' strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit={1.5}>
@@ -64,16 +101,16 @@ const LoadingSvg: React.FC<LoadingSvgProps> = () => {
                 <g id="loading" transform="matrix(0.104167,0,0,0.185185,5849,10.7829)">
                     <rect x="0" y="0" width="1920" height="1080" style={{ fill: 'none' }} />
                     <g transform="matrix(7.60717,-2.4705,4.392,4.27903,-44781.6,14763)">
-                        <path d="M6049,110.783C6049,165.974 6004.19,210.783 5949,210.783C5893.81,210.783 5849,165.974 5849,110.783C5849,55.591 5893.81,10.783 5949,10.783C5985.52,10.783 6017.5,30.406 6034.95,59.675" style={{ fill: 'none', stroke: 'black', strokeWidth: '15px' }} />
+                        <path className="main-color" d="M6049,110.783C6049,165.974 6004.19,210.783 5949,210.783C5893.81,210.783 5849,165.974 5849,110.783C5849,55.591 5893.81,10.783 5949,10.783C5985.52,10.783 6017.5,30.406 6034.95,59.675" style={{ fill: 'none', strokeWidth: '15px' }} />
                     </g>
                     <g transform="matrix(9.6,0,0,5.4,-56150.4,-58.2279)">
-                        <motion.path variants={successVariants} d="M5878,118C5914.22,137.222 5926.66,166.665 5926.66,166.665C5926.66,166.665 5937.44,137.606 5967.99,92.829C5998.54,48.051 6024,24 6024,24" style={{ fill: 'none', stroke: 'black', strokeWidth: '15px' }} />
+                        <motion.path className="main-color" variants={successVariants} d="M5878,118C5914.22,137.222 5926.66,166.665 5926.66,166.665C5926.66,166.665 5937.44,137.606 5967.99,92.829C5998.54,48.051 6024,24 6024,24" style={{ fill: 'none', strokeWidth: '15px' }} />
                     </g>
                     <g transform="matrix(9.6,0,0,-5.4,-56109.1,1175.58)">
-                        <motion.path variants={errorVariants} d="M5893,66C5893,66 5908.45,88.452 5938.99,118.99C5966.65,146.653 5992.98,165.98 5992.98,165.98" style={{ fill: 'none', stroke: 'black', strokeWidth: '15px' }} />
+                        <motion.path className="main-color" variants={errorVariants} d="M5893,66C5893,66 5908.45,88.452 5938.99,118.99C5966.65,146.653 5992.98,165.98 5992.98,165.98" style={{ fill: 'none', strokeWidth: '15px' }} />
                     </g>
                     <g transform="matrix(-9.6,0,0,-5.4,57996.3,1175.58)">
-                        <motion.path variants={errorVariants} d="M5893,66C5893,66 5908.45,88.452 5938.99,118.99C5966.65,146.653 5992.98,165.98 5992.98,165.98" style={{ fill: 'none', stroke: 'black', strokeWidth: '15px' }} />
+                        <motion.path className="main-color" variants={errorVariants} d="M5893,66C5893,66 5908.45,88.452 5938.99,118.99C5966.65,146.653 5992.98,165.98 5992.98,165.98" style={{ fill: 'none', strokeWidth: '15px' }} />
                     </g>
                 </g>
             </g>
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -27,44 +27,82 @@ const modalVariants: Variants = {
     }
 };
 
+const loaingTxtVariants: Variants = {
+    loading: {
+        y: '-100vh',
+        opacity: 0
+    },
+    error: {
+        y: '10vh',
+        opacity: 1,
+        transition: { delay: 0.5 }
+    },
+    success: {
+
+    }
+};
+let loadingParagraph: React.ReactNode = <p></p>;
+
 interface ModalProps { };
 const Modal: React.FC<ModalProps> = () => {
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [isSuccess, setISuccess] = useState(false);
     const dispatch = useAppDispatch();
     const showModal = useAppSelector((state) => state.modal.showModal);
     const form = useRef(null);
 
-    useEffect(() => {
-        console.log(showModal);
-    }, [showModal]);
+    if (isLoading) {
+        loadingParagraph = <p>{'Loading...'.split('').map((letter, index) => <motion.span style={{ whiteSpace: 'pre' }} key={`${index}-${letter}`} initial={{ y: 0 }} animate={{ y: 5, transition: { delay: index / 10, yoyo: Infinity } }}>{letter}</motion.span>)}</p>;
+    }
+
+    if (isError) {
+        loadingParagraph = <p>{'Oh oh, error sending the message'.split('').map((letter, index) => <motion.span style={{ whiteSpace: 'pre' }} key={`${index}-${letter}`} initial={{ y: 5 }} animate={{ y: 0, transition: { delay: index / 50, duration: 0.1 } }}>{letter}</motion.span>)}</p>;
+    }
+
+    if (isSuccess) {
+        loadingParagraph = <p>{'Message sent.'.split('').map((letter, index) => <motion.span style={{ whiteSpace: 'pre' }} key={`${index}-${letter}`} initial={{ y: 5 }} animate={{ y: 0, transition: { delay: index / 50, duration: 0.1 } }}>{letter}</motion.span>)}</p>;
+    }
 
     const sendEmail = (e: FormEvent) => {
         e.preventDefault();
+        setIsLoading(true);
 
-        if (form.current !== null) {
-            emailjs.sendForm('service_td928r5', 'template_kegh07c', form.current, 'FmDwq3RDXB_tZZNm0')
-                .then((result) => {
-                    console.log(result.text);
-                }, (error) => {
-                    console.log(error.text);
-                });
-        }
+        setTimeout(() => {
+            setIsError(true);
+            setTimeout(() => {
+                setIsError(false);
+                setISuccess(true);
+            }, 5000);
+        }, 5000);
+        // if (form.current !== null) {
+        //     emailjs.sendForm('service_td928r5', 'template_kegh07c', form.current, 'FmDwq3RDXB_tZZNm0')
+        //         .then((result) => {
+        //             console.log(result.text);
+        //         }, (error) => {
+        //             console.log(error.text);
+        //         });
+        // }
     };
 
     return (
         <AnimatePresence exitBeforeEnter>
             {showModal &&
                 <motion.div className='backdrop' variants={backdropVariants} animate='to' initial='from' exit='from'>
-                    <motion.div className='modal' variants={modalVariants}>
+                    <motion.div className='modal bkg' variants={modalVariants}>
                         {
-                            isLoading ? <LoadingSvg /> :
+                            isLoading ?
+                                <div className="loading-container">
+                                    <LoadingSvg isError={isError} isSuccess={isSuccess} />
+                                    {loadingParagraph}
+                                </div> :
                                 <form ref={form} onSubmit={sendEmail}>
                                     <label htmlFor="user_name">Name</label>
-                                    <input id="user_name" type="text" name="user_name" required />
+                                    <input className="bkg" id="user_name" type="text" name="user_name" required />
                                     <label htmlFor="user_email">Email</label>
-                                    <input id="user_email" type="email" name="user_email" required />
+                                    <input className="bkg" id="user_email" type="email" name="user_email" required />
                                     <label htmlFor="message">Message</label>
-                                    <textarea id="message" name="message" required />
+                                    <textarea className="bkg" id="message" name="message" required />
                                     <div>
                                         <button type="submit">SEND</button>
                                         <button className="cancel" type="button" onClick={() => dispatch(setShowModal(false))}>CANCEL</button>
